48e1e27768f60366ab731a7543be77ffd1326067
urielhaile <perenganito@gmail.com>
2022-05-24 14:20:48 -0500

    Limpieza del código fuente

--- src/App.js
+++ src/App.js
@@ -1,39 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { DescargarDatos, DescargarDatosFC } from './datos';
-import * as d3 from 'd3';
+import React from 'react';
+import {  DescargarDatosFC } from './datos';
 import { LineaTiempo } from './graficaTiempo';
 
 const width = 800;
 const height = 500;
-
-
-const Linea = ({datos}) => {
-const linea = d3.line()
-              .x((p) => p.x)
-              .y((p) => p.y);
-  return (<path d={linea(datos)} strokeWidth={1} fill="none" stroke="red" />);        
-};
-
-/*
-function App() {
-  const datos = DescargarDatos();  
-  const d = DescargarDatosFC();  
-  
-  if(!datos) {
-    return (
-      <svg width={width} height={height} >
-      </svg>
-    );
-  }
-  
-  return (
-    <svg width={width} height={height} >
-      <Linea datos={datos}/>
-    </svg>
-  );
-}
-*/
-
 
 function App() {
   const d = DescargarDatosFC();  
