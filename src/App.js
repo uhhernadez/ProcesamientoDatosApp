3e1a803068f5752c30fcc8b713087a8f06f9ed75
urielhaile <perenganito@gmail.com>
2022-05-24 14:40:08 -0500

    Parallax library

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import React from 'react';
-import {  DescargarDatosFC } from './datos';
-import { LineaTiempo } from './graficaTiempo';
-
-const width = 800;
-const height = 500;
+import {Parallax, ParallaxLayer} from 'react-spring';
 
 function App() {
-  const d = DescargarDatosFC();  
-
-  if(!d) {
-    return (
-    <div>
-      Loading..
-    </div>
-    );
-  }
-  
   return (
     <div>
-      <LineaTiempo data={d} width={width} height={height}/>
     </div>
   );
 }
-
 
 export default App;
